35313830686e9f46a279a7d7fe404583f07116dc
facinick <facinick@macmiller.local>
2023-01-21 21:09:04 +0530

    http -> https

--- src/client/frontent_apis/flight.ts
+++ src/client/frontent_apis/flight.ts
@@ -13,6 +13,6 @@ export const getFlightsFromToWhen = async ({
   to: FlightConnectedCityListType
   when: Date
 }): Promise<FlightsListType> => {
-  const response: AxiosResponse<Array<[string, FlightType[]]>> = await axios.get(`http://fast-flight-production.up.railway.app/api/search?from=${from}&to=${to}&when=${when}`);
+  const response: AxiosResponse<Array<[string, FlightType[]]>> = await axios.get(`https://fast-flight-production.up.railway.app/api/search?from=${from}&to=${to}&when=${when}`);
   return new Map(response.data)
 }
